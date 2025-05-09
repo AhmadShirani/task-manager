dd693c1730acec1c45a053e18c19af96d9ef024d
AhmadShirani <ah1sh2022@gmail.com>
2025-05-09 10:41:55 +0330

    update

---
+++ components/index.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="bg-[#181a1b] w-screen h-screen flex items-center justify-center px-10" dir="rtl">
+        <div class="sm">
+
+        </div>
+        <div>
+
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-screen text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                   عنوان وظیغه
+                </th>
+                <th scope="col" class="px-6 py-3">
+                   نام و نام خانوادگی
+                </th>
+                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                    وضعیت
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    شرح وظیفه
+                </th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4 overflow-hidden">
+                    احمد شیرانی
+                </td>
+                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Laptop
+                </td>
+                <NuxtLink to="">
+                    <td class="px-6 py-4">
+                   شرح
+                    </td>
+                </NuxtLink>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4 overflow-hidden">
+                    White
+                </td>
+                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Laptop PC
+                </td>
+                <NuxtLink to="">
+                    <td class="px-6 py-4">
+                   شرح
+                    </td>
+                </NuxtLink>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4 overflow-hidden">
+                    Black
+                </td>
+                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Accessories
+                </td>
+                <NuxtLink to="">
+                    <td class="px-6 py-4">
+                   شرح
+                    </td>
+                </NuxtLink>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Google Pixel Phone
+                </th>
+                <td class="px-6 py-4 overflow-hidden">
+                    Gray
+                </td>
+                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Phone
+                </td>
+                <NuxtLink to="">
+                    <td class="px-6 py-4">
+                   شرح
+                    </td>
+                </NuxtLink>
+            </tr>
+            <tr>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Apple Watch 5
+                </th>
+                <td class="px-6 py-4 overflow-hidden">
+                    Red
+                </td>
+                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                    Wearables
+                </td>
+                <NuxtLink to="">
+                    <td class="px-6 py-4">
+                   شرح
+                    </td>
+                </NuxtLink>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+        </div>
+    </div>
+</template>
